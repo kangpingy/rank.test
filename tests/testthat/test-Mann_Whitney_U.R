1380aeddc80f37da15c72ff3c79c13fd69e4ab32
Kangping Yang <kangpiny@umich.edu>
2019-11-24 23:30:44 -0500

    coverage case added

--- tests/testthat/test-Mann_Whitney_U.R
+++ tests/testthat/test-Mann_Whitney_U.R
@@ -3,6 +3,10 @@ test_that("Two small unpaired samples rank test statistics and p value is correc
   y <- rnorm(5)*2.5+2.5
   expect_equal(Mann_Whitney_U(x,y)[[1]], wilcox.test(x,y,correct = F)[[1]])  ## Compare Test statistics correctness
   expect_equal(Mann_Whitney_U(x,y)[[2]], wilcox.test(x,y,correct = F)[[3]])  ## Compare p value correctness
+  x <- rnorm(10)-2
+  y <- rnorm(15)+2
+  expect_equal(Mann_Whitney_U(x,y)[[1]], wilcox.test(x,y,correct = F)[[1]])  ## Compare Test statistics correctness
+  expect_equal(Mann_Whitney_U(x,y)[[2]], wilcox.test(x,y,correct = F)[[3]])  ## Compare p value correctness
 })
 
 test_that("Two large unpaired samples rank test statistics and p value is correct", {
@@ -18,7 +22,13 @@ test_that("One small unpaired sample rank test statistics and p value is correct
   expect_equal(Mann_Whitney_U(x)[[2]], wilcox.test(x,correct = F)[[3]])
 })
 
-test_that("Two small unpaired samples rank test statistics and p value is correct", {
+test_that("One small unpaired sample rank test statistics at median = 1 and p value is correct", {
+  x <- rnorm(20)
+  expect_equal(Mann_Whitney_U(x,median_test = 1)[[1]], wilcox.test(x,mu = 1, correct = F)[[1]])
+  expect_equal(Mann_Whitney_U(x, median_test = 1)[[2]], wilcox.test(x,mu = 1, correct = F)[[3]])
+})
+
+test_that("Two small unpaired samples rank test statistics with ties warning (p could not be caculated exactly)", {
   x <- c(rnorm(20)+0.5,2,2,2)
   y <- c(rnorm(12),1,1,1)
   expect_equal(Mann_Whitney_U(x,y)[[1]], wilcox.test(x,y,correct = F)[[1]])
@@ -27,7 +37,7 @@ test_that("Two small unpaired samples rank test statistics and p value is correc
   ##expect_equal(Mann_Whitney_U(x)[[2]], wilcox.test(x,correct = F)[[3]]) Ti
 })
 
-test_that("Two small unpaired samples rank test statistics and p value is correct", {
+test_that("One small sample rank test statistics with ties warning (p could not be caculated exactly)", {
   x <- c(rnorm(30)+1,2,2,2)
   expect_equal(Mann_Whitney_U(x)[[1]], wilcox.test(x,correct = F)[[1]])
   expect_warning(Mann_Whitney_U(x))
